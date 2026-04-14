26b632537d9b982de6eb9497d9578478489b60c9
Sameer More <sameermore@example.com>
2026-04-14 13:11:16 +0530

    added root route

--- server/index.js
+++ server/index.js
@@ -28,3 +28,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("Blog API is running 🚀");
+});
